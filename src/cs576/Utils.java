cf7d2844b8d60485942ed82262c3daade4345910
Jeffrey Ye <lightning.x.revenant@gmail.com>
2017-05-02 14:00:55 -0700

    speedup encoding

--- src/cs576/Utils.java
+++ src/cs576/Utils.java
@@ -216,12 +216,12 @@ public class Utils {
         }
     }
 
-//    static final float[][] temp = new float[DCT_BLOCK_LENGTH][DCT_BLOCK_LENGTH];
+    static final float[][] temp = new float[DCT_BLOCK_LENGTH][DCT_BLOCK_LENGTH];
     private static void forwardDCTBlock(float[][] image, int x, int y, boolean isLuminance,float[] output) {
         int height = image.length;
         int width = image[0].length;
 
-        float[][] temp = new float[DCT_BLOCK_LENGTH][DCT_BLOCK_LENGTH];
+//        float[][] temp = new float[DCT_BLOCK_LENGTH][DCT_BLOCK_LENGTH];
         copyToTemp(height, width, isLuminance, temp, x, y, image);
         DCT.forwardDCT(temp);
 
